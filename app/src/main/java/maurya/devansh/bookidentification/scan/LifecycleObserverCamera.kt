8e42c6f0295678df7235e2bd5802017805fc8473
Devansh Maurya <Devansh233@gmail.com>
2019-04-23 19:07:24 +0530

    Now accessing camera to capture image. Created a lifecycle observer to control CameraKit camera lifecycle

---
+++ app/src/main/java/maurya/devansh/bookidentification/scan/LifecycleObserverCamera.kt
@@ -0,0 +1,37 @@
+package maurya.devansh.bookidentification.scan
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.camerakit.CameraKitView
+
+/**
+ * Created by Devansh on 23/4/19.
+ */
+
+class LifecycleObserverCamera(lifecycle: Lifecycle, val cameraKitView: CameraKitView) : LifecycleObserver {
+
+    init {
+        lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onStart() {
+        cameraKitView.onStart()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onResume() {
+        cameraKitView.onResume()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onPause() {
+        cameraKitView.onPause()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onStop() {
+        cameraKitView.onStop()
+    }
+}
--- app/src/main/java/maurya/devansh/bookidentification/scan/ScanActivity.kt
+++ app/src/main/java/maurya/devansh/bookidentification/scan/ScanActivity.kt
@@ -2,12 +2,27 @@ package maurya.devansh.bookidentification.scan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_scan.*
 import maurya.devansh.bookidentification.R
 
 class ScanActivity : AppCompatActivity() {
 
+    private lateinit var lifecycleObserverCamera: LifecycleObserverCamera
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
+        lifecycleObserverCamera = LifecycleObserverCamera(this.lifecycle, camera)
+
+        captureButton.setOnClickListener {
+            camera.captureImage { cameraKitView, bytes ->
+
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
