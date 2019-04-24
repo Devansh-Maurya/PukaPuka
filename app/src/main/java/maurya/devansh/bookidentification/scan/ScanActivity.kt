416b457728870f178cb635e206ead5981923a759
Devansh Maurya <Devansh233@gmail.com>
2019-04-24 19:01:10 +0530

    Added code to rotate the captured image to get correct orientation

--- app/src/main/java/maurya/devansh/bookidentification/scan/ScanActivity.kt
+++ app/src/main/java/maurya/devansh/bookidentification/scan/ScanActivity.kt
@@ -1,9 +1,14 @@
 package maurya.devansh.bookidentification.scan
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_scan.*
+import android.graphics.Bitmap
+import com.camerakit.CameraKitView.CameraListener
 import maurya.devansh.bookidentification.R
+import java.io.ByteArrayOutputStream
+
 
 class ScanActivity : AppCompatActivity() {
 
@@ -12,17 +17,17 @@ class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-        lifecycleObserverCamera = LifecycleObserverCamera(this.lifecycle, camera)
+        lifecycleObserverCamera = LifecycleObserverCamera(this.lifecycle, cameraView)
 
         captureButton.setOnClickListener {
-            camera.captureImage { cameraKitView, bytes ->
-
+            cameraView.captureImage { cameraKitView, bytes ->
+                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
             }
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        cameraView.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
---
+++ app/src/main/java/maurya/devansh/bookidentification/util/RotationCalculator.kt
@@ -0,0 +1,69 @@
+package maurya.devansh.bookidentification.util
+
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Context.CAMERA_SERVICE
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
+import android.os.Build
+import android.util.Log
+import android.util.SparseIntArray
+import android.view.Surface
+import androidx.annotation.RequiresApi
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+
+/**
+ * Created by Devansh on 24/4/19.
+ */
+
+/**
+ * Get the angle by which an image must be rotated given the device's current
+ * orientation.
+ */
+
+class RotationCalculator {
+
+    private val ORIENTATIONS = SparseIntArray()
+
+    init {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90)
+        ORIENTATIONS.append(Surface.ROTATION_90, 0)
+        ORIENTATIONS.append(Surface.ROTATION_180, 270)
+        ORIENTATIONS.append(Surface.ROTATION_270, 180)
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Throws(CameraAccessException::class)
+    private fun getRotationCompensation(activity: Activity, context: Context): Int {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        val deviceRotation = activity.windowManager.defaultDisplay.rotation
+        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
+
+        // On most devices, the sensor orientation is 90 degrees, but for some
+        // devices it is 270 degrees. For devices with a sensor orientation of
+        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
+        val cameraManager = context.getSystemService(CAMERA_SERVICE) as CameraManager
+        val sensorOrientation = cameraManager
+            .getCameraCharacteristics("my camera id")
+            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
+        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360
+
+        // Return the corresponding FirebaseVisionImageMetadata rotation value.
+        val result: Int
+        when (rotationCompensation) {
+            0 -> result = FirebaseVisionImageMetadata.ROTATION_0
+            90 -> result = FirebaseVisionImageMetadata.ROTATION_90
+            180 -> result = FirebaseVisionImageMetadata.ROTATION_180
+            270 -> result = FirebaseVisionImageMetadata.ROTATION_270
+            else -> {
+                result = FirebaseVisionImageMetadata.ROTATION_0
+                Log.e("Bad Rotation", "Bad rotation value: $rotationCompensation")
+            }
+        }
+        return result
+    }
+}
