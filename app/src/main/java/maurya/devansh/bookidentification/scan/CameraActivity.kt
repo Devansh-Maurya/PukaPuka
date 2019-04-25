6dc8ed89181472683fd8dd33521fcfbc40770a06
Devansh Maurya <Devansh233@gmail.com>
2019-04-25 18:00:23 +0530

    Added a function to remove some sentences that contain some non-required words

--- app/src/main/java/maurya/devansh/bookidentification/scan/CameraActivity.kt
+++ app/src/main/java/maurya/devansh/bookidentification/scan/CameraActivity.kt
@@ -8,27 +8,24 @@ import android.util.Log
 import android.widget.Toast
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
-import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.google.firebase.ml.vision.text.FirebaseVisionText
 import maurya.devansh.bookidentification.R
+import java.lang.StringBuilder
 
 
 class CameraActivity : AppCompatActivity() {
 
-    private lateinit var lifecycleObservervableCamera: LifecycleObservervableCamera
+    private lateinit var lifecycleObservableCamera: LifecycleObservableCamera
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
-        lifecycleObservervableCamera = LifecycleObservervableCamera(this.lifecycle, cameraView)
+        lifecycleObservableCamera = LifecycleObservableCamera(this.lifecycle, cameraView)
 
         captureButton.setOnClickListener {
             cameraView.captureImage { cameraKitView, bytes ->
-                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-
-                previewImage.setImageBitmap(bitmap)
-
-                val image = FirebaseVisionImage.fromBitmap(bitmap)
-
+                val bitmapImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                val image = FirebaseVisionImage.fromBitmap(bitmapImage)
                 scanImage(image)
             }
         }
@@ -41,10 +38,52 @@ class CameraActivity : AppCompatActivity() {
 
     private fun scanImage(image: FirebaseVisionImage) {
         val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+
         val result = detector.processImage(image)
             .addOnSuccessListener {
-                Toast.makeText(this, it.text, Toast.LENGTH_SHORT).show()
-                Log.v("Text", it.text)
+                Toast.makeText(this, extractMeaningfulTextFromResult(it), Toast.LENGTH_SHORT).show()
+                Log.v("Text", extractMeaningfulTextFromResult(it))
             }
+
+
     }
+
+    private fun extractMeaningfulTextFromResult(result: FirebaseVisionText): String {
+        val stringBuilder = StringBuilder()
+
+        for (block in result.textBlocks) {
+            for (line in block.lines) {
+                val lineText = line.text
+
+                Log.v("lineText", lineText)
+
+                if (containsMeaningfulText(lineText))
+                    stringBuilder.append(lineText + '\n')
+            }
+        }
+
+        return stringBuilder.toString()
+    }
+
+    private fun containsMeaningfulText(string: String) =
+                !(string.contains("for sale", true)
+                        || string.contains("fer sale", true)
+                        || string.contains("far sale", true)
+                        || string.contains("nepal, bangla", true)
+                        || string.contains("bhutan only", true)
+                        || string.contains("edition", true)
+                        //Ignore all publishers names
+                        || string.contains("TATA McGRAW-HILL", true)
+                        || string.contains("McGraw-Hill", true)
+                        || string.contains("HarperCollins", true)
+                        || string.contains("Cengage", true)
+                        || string.contains("Wiley", true)
+                        || string.contains("Springer", true)
+                        || string.contains("scholastic", true)
+                        || string.contains("pearson", true)
+                        || string.contains("oxford", true)
+                        || string.contains("university press", true)
+                        || string.contains("cambridge", true)
+                        || string.contains("phi", true)
+                        )
 }
