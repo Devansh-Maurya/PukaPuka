c332e38cedbc5feac8f601c660373d5ef87180cd
Devansh Maurya <Devansh233@gmail.com>
2019-04-28 08:43:31 +0530

    Added an image view to show book cover and blurring it using Glide Blur Transformation

--- app/src/main/java/maurya/devansh/bookidentification/model/BookItem.kt
+++ app/src/main/java/maurya/devansh/bookidentification/model/BookItem.kt
@@ -11,7 +11,7 @@ data class BookItem(
     val authors: String = "",
     val publisher: String = "",
     val isbn13: String = "",
-    val categories: List<String> = listOf(),
+    val category: String = "",
     val rating: Double = 0.0,
     val thumbnailUrl: String = "",
     val description: String = ""
--- app/src/main/java/maurya/devansh/bookidentification/screens/bookinfo/BookInfoFragment.kt
+++ app/src/main/java/maurya/devansh/bookidentification/screens/bookinfo/BookInfoFragment.kt
@@ -12,6 +12,10 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import jp.wasabeef.glide.transformations.BlurTransformation
+import kotlinx.android.synthetic.main.fragment_book_info.view.*
 import maurya.devansh.bookidentification.R
 
 class BookInfoFragment : Fragment() {
@@ -21,7 +25,9 @@ class BookInfoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_book_info, container, false)
+        val view = inflater.inflate(R.layout.fragment_book_info, container, false)
+//        Blurry.with(context).radius(25).sampling(2).onto(view.frame.backgroundImage)
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,6 +35,13 @@ class BookInfoFragment : Fragment() {
 
         val queue = Volley.newRequestQueue(context)
         val url = "https://www.googleapis.com/books/v1/volumes/" + args.bookVolumeId
+
+        val requestOptions = RequestOptions()
+        requestOptions.transform(BlurTransformation(30))
+
+        Glide.with(context!!).load(R.drawable.sample)
+                .apply(requestOptions)
+                .into(view.backgroundImage)
 
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener {
