697d64923ed6be197dd104bf8409f2766bcd424b
Devansh Maurya <Devansh233@gmail.com>
2019-04-29 21:22:02 +0530

    Reduced text size on splash screen

--- app/src/main/java/maurya/devansh/bookidentification/MainActivity.kt
+++ app/src/main/java/maurya/devansh/bookidentification/MainActivity.kt
@@ -10,8 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-    }
+        setSupportActionBar(toolbar) }
 
     override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
 
