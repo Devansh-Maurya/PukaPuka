d0889f7d180d06c4d6846bbdb46d1f281018ac03
Devansh Maurya <Devansh233@gmail.com>
2019-04-29 20:31:32 +0530

    Added a SplashScreenFragment

---
+++ app/src/main/java/maurya/devansh/bookidentification/screens/splash/SplashScreenFragment.kt
@@ -0,0 +1,30 @@
+package maurya.devansh.bookidentification.screens.splash
+
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import maurya.devansh.bookidentification.R
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_appStart)
+        }, 5000)
+    }
+}
